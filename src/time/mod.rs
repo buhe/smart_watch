f79b8bebb83fae2fd8070b5f3407cc8528f59bf6
buhe <bugu1986@126.com>
2022-03-09 20:26:54 +0800

    u

--- src/time/mod.rs
+++ src/time/mod.rs
@@ -10,25 +10,27 @@ pub struct Time {
 
 impl App for Time {
     fn init(self: &Self, _ctx: &AppContext) -> Result<()> {
-        let mut stream = TcpStream::connect("one.one.one.one:80")?;
+        println!("hello time");
+        let mut stream = TcpStream::connect("36.152.44.96:80")?;
 
-    let err = stream.try_clone();
-    if let Err(err) = err {
+        // let err = stream.try_clone();
+        // if let Err(err) = err {
+        //     println!(
+        //         "Duplication of file descriptors does not work (yet) on the ESP-IDF, as expected: {}",
+        //         err
+        //     );
+        // }
+
+        stream.write_all("GET / HTTP/1.0\n\n".as_bytes())?;
+
+        let mut result = Vec::new();
+
+        stream.read_to_end(&mut result)?;
+
         println!(
-            "Duplication of file descriptors does not work (yet) on the ESP-IDF, as expected: {}",
-            err
-        );
-    }
-
-    stream.write_all("GET / HTTP/1.0\n\n".as_bytes())?;
-
-    let mut result = Vec::new();
-
-    stream.read_to_end(&mut result)?;
-
-    println!(
-        "1.1.1.1 returned:\n=================\n{}\n=================\nSince it returned something, all is OK",
-        std::str::from_utf8(&result)?);
+            "ntp returned:\n=================\n{}\n=================\nSince it returned something, all is OK",
+            std::str::from_utf8(&result)?);
+        println!("hello time end");
         Ok(())
     }
 
